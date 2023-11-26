368e868da101ed291080e881b62f5655fc7699d3
Praveen <satyammaurya9620@gmail.com>
2023-11-26 15:57:42 +0530

    added some cool feature

--- 3d-cool-text/src/script.js
+++ 3d-cool-text/src/script.js
@@ -2,12 +2,13 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import {FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry'
-/**
- * Base
- */
-// Debug
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+alert("Welcome to the future which will predict your bf/gf name....")
+const a = prompt("Hey please enter your beautiful name!!")
+alert("Touch the bubble to burst out in order to search your bf/gf name ")
+
+// Base
 const gui = new dat.GUI()
 
 // Canvas
@@ -16,97 +17,120 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Textures
- */
-
+// Textures
 const textureLoader = new THREE.TextureLoader()
-const matcaptexture=textureLoader.load('./textures/matcaps/1.png')
-const matcaptexture1=textureLoader.load('./textures/matcaps/8.png')
-const fontloader=new FontLoader()
+const matcaptexture = textureLoader.load('./textures/matcaps/1.png')
+const matcaptexture1 = textureLoader.load('./textures/matcaps/8.png')
+const fontloader = new FontLoader()
+
 fontloader.load(
     '/fonts/helvetiker_regular.typeface.json',
-    (font)=>{
-         const textGeometry=new TextGeometry(
-            'Praveen Maurya',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            "Click to reveal the name ",
             {
-                font:font,
-                size:0.5,
-                height:0.2,
-                curveSegments:12,
-                bevelEnabled:true,
-                bevelThickness:0.03,
-                bevelSize:0.02,
-                bevelOffset:0,
-                bevelSegments:5,
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5,
             }
-         )
-        //  textGeometry.computeBoundingBox();
-        //  textGeometry.translate(
-        //  -textGeometry.boundingBox.max.x*0.5,
-        //  -textGeometry.boundingBox.max.y*0.5,
-        //  -textGeometry.boundingBox.max.z*0.5
-        //  )
+        )
+
         textGeometry.center()
-         const textMaterial=new THREE.MeshMatcapMaterial({matcap:matcaptexture})
-         const text=new THREE.Mesh(textGeometry,textMaterial)
-         scene.add(text)
-         let donutgeometry=new THREE.TorusGeometry(0.3,0.2,20,45)
-         const donusmateral=new THREE.MeshMatcapMaterial({matcap:matcaptexture1})
-        for(let i=0;i<=1000;i++){
-           
-            const donut=new THREE.Mesh(donutgeometry,donusmateral)
+        const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcaptexture })
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
+
+        let donutgeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        const donusmateral = new THREE.MeshMatcapMaterial({ matcap: matcaptexture1 })
+
+        const donuts = []
+
+        for (let i = 0; i <= 1000; i++) {
+            const donut = new THREE.Mesh(donutgeometry, donusmateral)
             scene.add(donut)
-            donut.position.x=(Math.random()-0.5)*10
-            donut.position.y=(Math.random()-0.5)*10
-            donut.position.z=(Math.random()-0.5)*10
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
 
-            donut.rotation.x=Math.random()*Math.PI
-            donut.rotation.y=Math.random()*Math.PI
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
 
-            const scale=Math.random()
-            donut.scale.set(scale,scale,scale)
-         }
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            donuts.push(donut)
+        }
+
+        const raycaster = new THREE.Raycaster()
+        const raycasterText = new THREE.Raycaster();
+        const mouse = new THREE.Vector2()
+
+        window.addEventListener('mousemove', (event) => {
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+            raycaster.setFromCamera(mouse, camera)
+
+            const intersects = raycaster.intersectObjects(donuts)
+
+            if (intersects.length > 0) {
+                const selectedDonut = intersects[0].object
+                scene.remove(selectedDonut)
+                donuts.splice(donuts.indexOf(selectedDonut), 1)
+            }
+        })
+
+        window.addEventListener('click', (event) => {
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+            raycaster.setFromCamera(mouse, camera)
+
+            const intersectsDonuts = raycaster.intersectObjects(donuts)
+            if (intersectsDonuts.length > 0) {
+                const selectedDonut = intersectsDonuts[0].object
+                scene.remove(selectedDonut)
+                donuts.splice(donuts.indexOf(selectedDonut), 1)
+            }
+
+            raycasterText.setFromCamera(mouse, camera);
+            const intersectsText = raycasterText.intersectObject(text);
+
+            if (intersectsText.length > 0) {
+                prompt("Do you like somebody?")
+                prompt("Type the first letter of your crush name..")
+                alert("Click ok to get your partner name")
+                alert("Abe chutiye kya karega name janke ...pategi/patega toh waise bhi nhi...face dekha/dekhi  hai apna/apni....")
+            }
+        })
+
     }
 )
 
-/**
- * Object
- */
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial()
-// )
-
-// scene.add(cube)
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
+window.addEventListener('resize', () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
@@ -117,31 +141,23 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
     controls.update()
 
-    // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
